79e17e50a7825a56665e6bd728d97bf19f73c707
kasun <kasunsiyambalapitiya@gmail.com>
2017-03-09 22:26:39 +0530

    Add javadoc for MainClass.java

--- CodeQualityMatricesProject/src/main/java/com/wso2/code/quality/matrices/MainClass.java
+++ CodeQualityMatricesProject/src/main/java/com/wso2/code/quality/matrices/MainClass.java
@@ -1,39 +1,35 @@
 package com.wso2.code.quality.matrices;
 
 import org.json.JSONArray;
-import java.util.Scanner;
+
 import java.util.Set;
 
+/**
+ * This is the class having the main method of this application
+ * PMT Access token, patch id and github access token
+ * should be passed as command line arguments when running the application
+ */
 public class MainClass {
     public static void main(String[] args) {
-      //  System.out.println("Enter PMT Token");
-        String pmtToken = args[0];
 
-       // System.out.println("Enter the patch id");
+        String pmtToken = args[0];
         String patchId = args[1];
 
         String pmtUrl = "http://umt.private.wso2.com:9765/codequalitymatricesapi/1.0.0//properties?path=/_system/governance/patchs/" + patchId;
 
-
         RestApiCaller restApiCaller = new RestApiCaller();
         JSONArray jsonArray = (JSONArray) restApiCaller.callingTheAPI(pmtUrl, pmtToken, false, false);
-
 
         Pmt pmt = new Pmt();
         String[] commitsInTheGivenPatch = pmt.getThePublicGitCommitId(jsonArray);
 
-        //        passing the github token
-       // System.out.println("Enter Github token");
         String gitHubToken = args[2];
-
 
         BlameCommit blameCommit = new BlameCommit();
         Set<String> commitHashObtainedForPRReview = blameCommit.obtainingRepoNamesForCommitHashes(gitHubToken, commitsInTheGivenPatch, restApiCaller);
 
         Reviewers reviewers = new Reviewers();
         reviewers.findingReviewers(commitHashObtainedForPRReview, gitHubToken);
-
-
 
 
     }
