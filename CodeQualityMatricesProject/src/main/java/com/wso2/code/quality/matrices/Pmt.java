08698a824760248e6db90f87dd28dd20517affba
kasun <kasunsiyambalapitiya@gmail.com>
2017-03-09 22:27:05 +0530

    Add javadoc for Pmt.class

--- CodeQualityMatricesProject/src/main/java/com/wso2/code/quality/matrices/Pmt.java
+++ CodeQualityMatricesProject/src/main/java/com/wso2/code/quality/matrices/Pmt.java
@@ -19,57 +19,43 @@ package com.wso2.code.quality.matrices;/*
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+/**
+ * This class is used for obtaining the commit hashes that belongs to the given patch
+ */
+
 public class Pmt {
     private String[] patchInformation_svnRevisionpublic;
 
 
     /**
-     * getting the commit IDs from the above saved file
+     * getting the commit IDs from the received json response
+     *
+     * @param jsonArray jsonarray containing the output received from WSO2 PMT for the given patch
+     * @return a copy of the array containing the commit hashes that belongs to the given patch
      */
+
     public String[] getThePublicGitCommitId(JSONArray jsonArray) {
-
-
         for (int i = 0; i < jsonArray.length(); i++) {
             JSONObject jsonObject = (JSONObject) jsonArray.get(i);
-
             String tempName = (String) jsonObject.get("name");
-
             if (tempName.equals("patchInformation_svnRevisionpublic")) {
                 JSONArray tempCommitsJSONArray = (JSONArray) jsonObject.get("value");
-
                 //initializing the patchInformation_svnRevisionpublic array
-
                 patchInformation_svnRevisionpublic = new String[tempCommitsJSONArray.length()];
-
                 for (int j = 0; j < tempCommitsJSONArray.length(); j++) {
-
-
                     patchInformation_svnRevisionpublic[j] = ((String) tempCommitsJSONArray.get(j)).trim();     // for ommiting the white spaces at the begingin and end of the commits
-
-
                 }
-
                 System.out.println("The commit Ids are");
-
-
                 //            for printing all the commits ID associated with a patch
                 for (String tmp : patchInformation_svnRevisionpublic) {
                     System.out.println(tmp);
                 }
                 System.out.println();
-
-
-
                 break;
             }
-
-
         }
         //to prevent from internaal representation by returning referecnce to mutable object
-        String clonedPatchInformation_svnRevisionpublic[]= patchInformation_svnRevisionpublic.clone();
-
+        String clonedPatchInformation_svnRevisionpublic[] = patchInformation_svnRevisionpublic.clone();
         return clonedPatchInformation_svnRevisionpublic;
-
-
     }
 }
