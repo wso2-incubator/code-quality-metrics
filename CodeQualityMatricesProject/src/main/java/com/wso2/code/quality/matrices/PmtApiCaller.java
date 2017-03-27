b218275e58d27e61d0e7a563fb930858414a4c3c
kasun <kasun.siyambalapitiya@gmail.com>
2017-03-27 13:08:11 +0530

    Seperate PMT communication to a seperate class

---
+++ CodeQualityMatricesProject/src/main/java/com/wso2/code/quality/matrices/PmtApiCaller.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.wso2.code.quality.matrices;
+
+import org.apache.http.client.methods.HttpGet;
+
+public class PmtApiCaller {
+    public PmtApiCaller() {
+
+    }
+
+    /**
+     * @param accessToken
+     * @param patchId
+     * @return
+     * @throws CodeQualityMatricesException
+     */
+    public String callApi(String accessToken, String patchId) throws CodeQualityMatricesException {
+        String pmtUrl = "http://umt.private.wso2.com:9765/codequalitymatricesapi/1.0.0//properties?path=/_system/governance/patchs/" + patchId;
+        HttpGet httpGet;
+        try {
+            httpGet = new HttpGet(pmtUrl);
+            httpGet.addHeader("Authorization", "Bearer " + accessToken);
+
+        } catch (IllegalArgumentException e) {
+            throw new CodeQualityMatricesException("The url provided for accessing the PMT API is invalid ", e);
+        }
+
+        return ApiUtility.callApi(httpGet);
+    }
+}
