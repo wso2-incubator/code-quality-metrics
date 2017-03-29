9dc9c9d83135616119e7363d16cd172eed0055e9
kasun <kasun.siyambalapitiya@gmail.com>
2017-03-29 15:37:14 +0530

    Add model classes

---
+++ CodeQualityMatricesProject/src/main/java/com/wso2/code/quality/matrices/model/GraphqlRange.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.wso2.code.quality.matrices.model;
+
+public class GraphqlRange {
+    private int startingLine;
+    private int endingLine;
+    private int age;
+    private Commit commit;
+
+    public int getStartingLine() {
+        return startingLine;
+    }
+
+    public void setStartingLine(int startingLine) {
+        this.startingLine = startingLine;
+    }
+
+    public int getEndingLine() {
+        return endingLine;
+    }
+
+    public void setEndingLine(int endingLine) {
+        this.endingLine = endingLine;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Commit getCommit() {
+        return commit;
+    }
+
+    public void setCommit(Commit commit) {
+        this.commit = commit;
+    }
+}
